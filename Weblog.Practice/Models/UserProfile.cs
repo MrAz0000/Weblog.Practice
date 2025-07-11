025401036e9dd8073eda6edf953ddfb3cf32237d
MrAz0000 <164420321+MrAz0000@users.noreply.github.com>
2025-07-11 10:41:47 -0700

    Add project files.

---
+++ Weblog.Practice/Models/UserProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace Weblog.Practice.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        public string ?ProfileUrl { get; set; }
+        public long bio { get; set; }
+        public List<string> ?SocialLinks { get; set; }
+        public User ?User { get; set; }
+    }
+}
