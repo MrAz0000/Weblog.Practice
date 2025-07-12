8344cd176aed53221038f8cd5b8d56f52c66e2d0
MrAz0000 <164420321+MrAz0000@users.noreply.github.com>
2025-07-12 13:30:50 -0700

    Login and register added

---
+++ Weblog.Practice/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using Weblog.Practice.Data;
+using Weblog.Practice.Models;
+
+namespace Weblog.Practice.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly WeblogDbContext context;
+
+        public AccountController(WeblogDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            if (ModelState.IsValid && user is not null)
+            {
+                var usez = context.Users.FirstOrDefault((x) => x.Username == user.Username && x.Password == user.Password);
+                if (usez != null)
+                {
+                    return RedirectToAction("Index", "Home");
+
+                }
+            }
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Users.Add(user);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Error = "somthing went wrong";
+                return View();
+            }
+        }
+    }
+}
--- Weblog.Practice/Models/User.cs
+++ Weblog.Practice/Models/User.cs
@@ -1,9 +1,15 @@
-﻿namespace Weblog.Practice.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Weblog.Practice.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [Length(5,25 , ErrorMessage = "Username must be betwwen 5-25 words")]
         public string? Username { get; set; }
+        [Required]
+        [MinLength(8)]
         public string? Password { get; set; }
         public UserProfile? Profile { get; set; }
 
